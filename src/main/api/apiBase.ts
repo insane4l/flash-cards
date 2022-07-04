f0b3301c720ab7e057ca3e35b16b45a77a39d29c
Vlad Kravets <hazardvlad19@gmail.com>
2022-07-04 20:57:43 +0300

    fixed baseURL

--- src/main/api/apiBase.ts
+++ src/main/api/apiBase.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const apiBase = axios.create({
     // baseURL: 'https://neko-back.herokuapp.com/2.0',
-    baseURL: process.env.REACT_APP_BACK_URL || 'https://neko-back.herokuapp.com/2.0/',
+    baseURL:'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
