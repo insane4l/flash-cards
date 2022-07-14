db935e5274f023359651e067f1816a8cfd8843d1
Yauheni Kadyrau <7077634@gmail.com>
2022-07-14 12:13:20 +0300

    add modal for edit del

--- src/features/cards/PacksList/PacksTable/PacksTable.tsx
+++ src/features/cards/PacksList/PacksTable/PacksTable.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
@@ -14,9 +14,11 @@ import s from './PacksTable.module.css'
 import {PackType} from '../../../../main/api/packListAPI';
 import Spinner from '../../../../main/ui/common/Spinner/Spinner';
 import trash from '../../../../assets/icons/delete.png'
-import editt from '../../../../assets/icons/edit.svg'
+import edit from '../../../../assets/icons/edit.svg'
 import learn from '../../../../assets/icons/package.png'
 import ModalWindow from "../../../../main/ui/common/ModalWindow/ModalWindow";
+import SuperButton from "../../../../main/ui/common/SuperButton/SuperButton";
+import EditableTextLine from "../../../../main/ui/common/EditableTextLine/EditableTextLine";
 
 
 export const PacksTable: FC<PacksTablePropsType> = React.memo(({packs, authUserId, isLoading}) => {
@@ -25,29 +27,28 @@ export const PacksTable: FC<PacksTablePropsType> = React.memo(({packs, authUserI
         <PacksTableRow key={pack._id} pack={pack} isOwner={authUserId === pack.user_id}/>
     ))
 
-    if (isLoading) return <Spinner />
+    if (isLoading) return <Spinner/>
 
     return (
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 500}} aria-label="custom pagination table">
 
-                    <PacksTableHead />
+                <PacksTableHead/>
 
-                    <TableBody>
-                        { tableRows }
-                    </TableBody>
+                <TableBody>
+                    {tableRows}
+                </TableBody>
 
-                </Table>
-            </TableContainer>
+            </Table>
+        </TableContainer>
     )
 })
-
 
 
 const PacksTableHead = () => {
     return (
         <TableHead>
-            <TableRow style={{backgroundColor: '#F9F9FE' }}>
+            <TableRow style={{backgroundColor: '#F9F9FE'}}>
                 <TableCell align="left"> Name</TableCell>
                 <TableCell align="center">Total cards</TableCell>
                 <TableCell align="center">Last updated</TableCell>
@@ -59,20 +60,35 @@ const PacksTableHead = () => {
 }
 
 
-
 const PacksTableRow: FC<PacksTableRowPropsType> = ({pack, isOwner}) => {
+
+    const name = pack.name
+
+    const [editModalMode, setEditModalMode] = useState(false)
+    const [deleteModalMode, setDeleteModalMode] = useState(false)
+    const [newName, setNewName] = useState(name)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+
+    const openModal = () => setEditModalMode(true)
+    const closeModal = () => setEditModalMode(false)
+
+    const openDelModal = () => setDeleteModalMode(true)
+    const closeDelModal = () => setDeleteModalMode(false)
 
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
     const cardsListPath = PATH.cardsList.replace(/:packId/, '')
 
+    useEffect(() => {
+        (name === newName) ? setBtnDisabled(true) : setBtnDisabled(false)
+    }, [name, newName])
     const deletePackHandler = (packId: string) => {
         dispatch(deletePackTC(packId))
     }
 
-    const editPackNameHandler = (packId: string, name: string) => {
-        dispatch(editPackTC(packId, name))
-        return <Navigate to={'/cards'} />
+    const editPackNameHandler = (packId: string) => {
+        dispatch(editPackTC(packId, newName))
+        return <Navigate to={'/cards'}/>
     }
 
     const learnPackHandler = (packId: string) => {
@@ -87,40 +103,48 @@ const PacksTableRow: FC<PacksTableRowPropsType> = ({pack, isOwner}) => {
                     {pack.name}
                 </NavLink>
             </TableCell>
-            <TableCell style={{ width: 170 }} align="center">
+            <TableCell style={{width: 170}} align="center">
                 {pack.cardsCount}
             </TableCell>
-            <TableCell style={{ width: 170 }} align="center">
+            <TableCell style={{width: 170}} align="center">
                 {pack.updated.slice(0, 10)}
             </TableCell>
 
-            <TableCell style={{ width: 170 }} align="center">
+            <TableCell style={{width: 170}} align="center">
                 {pack.user_name}
             </TableCell>
-            <TableCell style={{ width: 300 }} align="center">
+            <TableCell style={{width: 300}} align="center">
 
 
-                <img className={s.active_icon} src={learn}  onClick={() => learnPackHandler(pack._id)}/>
+                <img className={s.active_icon} src={learn} onClick={() => learnPackHandler(pack._id)}/>
                 {isOwner
                     ? <>
 
-                        <img className={s.active_icon} src={editt} onClick={() => {
+                        <img className={s.active_icon} src={edit} onClick={openModal}/>
 
-                            editPackNameHandler(pack._id, pack.name)}}/>
-                        <img className={s.active_icon} src={trash} onClick={() => {
-                            deletePackHandler(pack._id)}} alt='delete button' />
+                        <img className={s.active_icon} src={trash} onClick={openDelModal} alt='delete button'/>
                     </>
 
                     : undefined}
 
+                <ModalWindow open={editModalMode} onClose={closeModal} title={'Change pack Name'}>
+                    <EditableTextLine text={newName} setNewText={setNewName}/>
+                    <SuperButton btnStyle="outline_danger" onClick={closeModal}>Cancel</SuperButton>
+                    <SuperButton onClick={() => editPackNameHandler(pack._id)}
+                                 disabled={btnDisabled}>Save</SuperButton>
+                </ModalWindow>
 
+                <ModalWindow open={deleteModalMode} onClose={closeDelModal} title={'Do you really want to delete pack'}>
 
+                    <SuperButton btnStyle="outline_danger" onClick={closeDelModal}>Cancel</SuperButton>
+                    <SuperButton onClick={() => deletePackHandler(pack._id)}
+                    >Delete</SuperButton>
+                </ModalWindow>
 
             </TableCell>
         </TableRow>
     )
 }
-
 
 
 type PacksTablePropsType = {
