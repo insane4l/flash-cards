8ec8d2f57bca990ffdd53e000b015daa11894dda
insane4l <43768644+insane4l@users.noreply.github.com>
2022-07-23 02:07:08 +0300

    AroundThePlanet spinner created. Rocket styles with auto-size fixed

--- src/main/ui/common/Spinner/Spinner.tsx
+++ src/main/ui/common/Spinner/Spinner.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { Rocket } from '../FlyingRocket/Rocket/Rocket'
 import s from './Spinner.module.css'
 
-const Spinner = () => {
+const Spinner: FC<SpinnerPropsType> = ({className = ''}) => {
+
     return (
-        <div className={s.loading}>
-            <div className={s.arc}></div>
-            <div className={s.arc}></div>
-            <div className={s.arc}></div>
+        <div className={s.wrapper + ' ' + className}>
+            <div className={s.planet}>
+                <div className={s.crater + ' ' + s.crater1}></div>
+                <div className={s.crater + ' ' + s.crater2}></div>
+                <div className={s.crater + ' ' + s.crater3}></div>
+                <div className={s.crater + ' ' + s.crater4}></div>
+                <div className={s.crater + ' ' + s.crater5}></div>
+
+                <div className={s.planetShadow}></div>
+            </div>
+
+            <div className={s.orbit}>
+                <Rocket withFlame className={s.rocket} />
+            </div>
         </div>
     )
 }
 
 export default Spinner
+
+
+type SpinnerPropsType = {
+    /** You can set Spinner width & height in css
+     * other parts of Spinner will have an auto-size
+    */
+   className?: string
+}
